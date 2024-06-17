b0a0dba0ea476a4050584cdc3d04f8ae6dbf4bf6
luisrh95 <luisrh-95@hotmail.com>
2024-06-17 15:27:24 -0500

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -8,64 +8,155 @@ function cargarListaDesdeLocalStorage() {
   if (listaGuardada) {
     listaCompras = JSON.parse(listaGuardada);
   }
+	
+
+	
 }
 
 // Función para guardar la lista en localStorage
 function guardarListaEnLocalStorage() {
   localStorage.setItem('listaCompras', JSON.stringify(listaCompras));
+  
 }
 
-// Función para agregar un item a la lista
-function agregarItem() {
-  let nombre = PreguntaLoop("Ingrese el nombre del artículo:");
-  let cantidad = PreguntaLoop("Ingresa la cantidad que necesitas");
+
+document.querySelector("#addItem").addEventListener("submit", event => {
+  event.preventDefault();
 	
-  if (nombre && cantidad) { // Comprueba si se ingresaron valores
-    let item = { nombre, cantidad };
+  const {elements} = event.target;
+
+  
+  const nombre = elements["producto"].value;
+  const cantidad = elements["cantidad"].value;
+  const volumen = elements["volumen"].value;
+	
+  if (nombre && cantidad && volumen) { // Comprueba si se ingresaron valores
+	  
+    let item = { nombre, cantidad,volumen };
+	  
     listaCompras.push(item);
     actualizarLista();
     guardarListaEnLocalStorage();
+	Swal.fire(""+nombre+" Se agregó a tu lista");
+	  
+	document.getElementById("addItem").reset();
+	  
   }else{
-	  alert('No se agregaron nuevos item a tu lista de mercado');
+	  Swal.fire("Debes completar toda la información");
+	  
   }
-	
-}
+
+ 
+});
 
 // Función para eliminar un item de la lista del mercado
 function eliminarItem(elemento, item) {
   // Preguntamos si está seguro de borrar el item del mercado
-  if (confirm('¿Deseas borrar ' + item.nombre + ' del mercado?') == true) {
-    listaCompras.splice(elemento, 1);
-    actualizarLista();
-    guardarListaEnLocalStorage();
-  } else {
-    alert('Atención - ' + item.nombre + ' no se borró del mercado');
-  }
+	Swal.fire({
+	  title: '¿Deseas borrar '+item.nombre+' del mercado?',
+	  showDenyButton: true,
+	  confirmButtonText: "Borrar",
+	  denyButtonText: `No borrar`
+	}).then((result) => {
+	  /* Read more about isConfirmed, isDenied below */
+	  if (result.isConfirmed) {
+		Swal.fire(""+item.nombre+" se ha borrado", "", "success");
+		 
+		 listaCompras.splice(elemento, 1);
+    	 actualizarLista();
+    	 guardarListaEnLocalStorage();  
+		  
+		  
+	  } else if (result.isDenied) {
+		Swal.fire(""+item.nombre+" no se borro de tu lista ", "", "info");
+	  }
+	});
+	
+	
 }
 
 // Función actualizar un item de la lista del mercado
 function actualizarItem(elemento, item) {
-  // Preguntamos si está seguro de borrar el item del mercado
-  if (confirm('¿Deseas actualizar ' + item.nombre + ' del mercado?') == true) {
-    
-  let NewNombre = PreguntaLoop("Ingrese el nuevo nombre del artículo:", item.nombre);
-  let NewCantidad = PreguntaLoop("Ingresa la nueva cantidad que necesitas", item.cantidad);	  
+  //Preguntamos si está seguro de actualizar el item
+	Swal.fire({
+	  title: '¿Deseas editar '+item.nombre+' del mercado?',
+	  showDenyButton: true,
+	  confirmButtonText: "Editar",
+	  denyButtonText: `Cancelar`
+	}).then((result) => {
+	  /* Read more about isConfirmed, isDenied below */
+	  if (result.isConfirmed) {
+		//proceso para modificar el item
+		let itemSelect = document.querySelector('.item_'+elemento+'');
+
+		
+        let FormUpdate = `
+          
+			  <input type="text" class="Nproducto" name="Nproducto" placeholder="Nuevo nombre"/>
+			  <input type="number" class="Ncantidad" name="Ncantidad" placeholder="Cantidad"/>
+			  <select class="ListVolumen Nvolumen" name="Nvolumen">
+				  <option value="">Volumen</option>
+			  </select>
+
+
+		`;
+		  
+		  
+		itemSelect.children[0].className = 'FormsUpdate';
+		itemSelect.children[0].innerHTML = FormUpdate;
+		  
+		PrintListV()
+		
+    	let botonSave = document.createElement("button");
+    	let botonCancel = document.createElement("button");
 	  
-  if (NewNombre && NewCantidad) { // Comprueba si se ingresaron valores
-       listaCompras.forEach(function(elemento){
-        item.nombre = NewNombre;
-        item.cantidad = NewCantidad;
-       }) 
-	  actualizarLista();
-      guardarListaEnLocalStorage();
-  }else{
-	  alert('Atención - ' + item.nombre + ' no se actualizó');
-  } 
+    	botonSave.textContent = 'Guardar';
+		botonCancel.textContent = "Cancelar";
 	  
+		botonCancel.onclick = function() {
+		    Swal.fire(""+item.nombre+" No se modificó ", "", "info");
+			actualizarLista();
+		};
 	  
-  } else {
-    alert('Atención - ' + item.nombre + ' no se actualizó');
-  }
+		botonSave.onclick = function() {
+		  //actualizarItem(elemento, item);
+		  let Nname = document.querySelector('[name=Nproducto]').value
+		  let Ncantidad = document.querySelector('[name=Ncantidad]').value
+		  let Nvolumen = document.querySelector('[name=Nvolumen]').value
+		  
+		  if (Nname && Ncantidad && Nvolumen) { // Comprueba si se ingresaron valores
+			   listaCompras.forEach(function(elemento){
+				item.nombre = Nname;
+				item.cantidad = Ncantidad;
+				item.volumen = Nvolumen;
+			   }) 
+			  actualizarLista();
+			  guardarListaEnLocalStorage();
+			  
+			  Swal.fire(""+Nname+" Se modificó con éxito", "", "info");
+			  
+		  }else{
+			  Swal.fire("Debes completar todos los campos", "", "info");
+		  } 
+		  
+		  
+		};
+		  
+		 
+		[].forEach.call(document.querySelectorAll(".btnList"), function(btnList){
+			btnList.parentNode.removeChild(btnList);
+		});
+		  
+		  
+		itemSelect.appendChild(botonSave);
+		itemSelect.appendChild(botonCancel);
+		  
+		  
+	  } else if (result.isDenied) {
+		//Swal.fire(""+item.nombre+" no se borro de tu lista ", "", "info");
+	  }
+	});
+	
 }
 
 // Función para actualizar la lista del mercado #lista-mercado
@@ -73,48 +164,110 @@ function actualizarLista() {
   let lista = document.getElementById("lista-mercado");
   // Borra los elementos de la lista para evitar duplicados
   lista.innerHTML = "";
-  // Recorre el array y agrega cada elemento como un <li> a la lista
-  listaCompras.forEach((item, elemento) => {
-    let li = document.createElement("li");
-    li.innerHTML = '<p><strong>'+item.nombre+'</strong> - Cantidad: <span>'+item.cantidad+'</span></p>';
-    // Agrega un botón para eliminar el elemento
-    let botonBorrar = document.createElement("button");
-    let botonEditar = document.createElement("button");
+  
+  // Verificamos que el array no este vacio, si lo esta imprimimos imagen en la lista
+	
+  if(listaCompras.length > 0){
 	  
-    botonEditar.textContent = 'Editar';
-	botonBorrar.textContent = "X";
+	  // Recorre el array y agrega cada elemento como un <li> a la lista
+	  listaCompras.forEach((item, elemento) => {
+		let li = document.createElement("li");
+		li.classList.add('item_'+elemento+'')
+		li.innerHTML = '<div><p><strong>'+item.nombre+'</strong> - Cantidad: <span>'+item.cantidad+'</span><img src="./images/icon_'+item.volumen+'.png" title=""/></p></div>';
+		// Agrega un botón para eliminar el elemento
+		let botonBorrar = document.createElement("button");
+		let botonEditar = document.createElement("button");
+
+		botonEditar.className = 'btnList';
+		botonEditar.textContent = 'Editar';
+		botonBorrar.textContent = "X";
+		botonBorrar.className = 'btnList';
+
+		botonBorrar.onclick = function() {
+		  eliminarItem(elemento, item);
+		};
+
+		botonEditar.onclick = function() {
+		  actualizarItem(elemento, item);
+		};
+
+
+		li.appendChild(botonEditar);
+		li.appendChild(botonBorrar);
+
+		lista.appendChild(li);
+	  });
 	  
-    botonBorrar.onclick = function() {
-      eliminarItem(elemento, item);
-    };
-	  
-    botonEditar.onclick = function() {
-      actualizarItem(elemento, item);
-    };
-	  
-    
-    li.appendChild(botonEditar);
-	li.appendChild(botonBorrar);
-	  
-    lista.appendChild(li);
-  });
+  }else{
+	  let li = document.createElement("li");
+	  li.className = 'CompraVacia'
+	  li.innerHTML = '<img src="./images/compra.png" title="Tu lista esta vacia, agrega un item a tu canasta de compras"/>';
+	  lista.appendChild(li);
+  }
+	
+	
+
 }
 
-// Esta función nos permite generar un loop por pregunta si el usuario envia un null
-function PreguntaLoop(TextPregunta, defaultTxt) {
-  let retval;
-  while (true) {
-    retval = (prompt(TextPregunta,defaultTxt));
-    
-    if (retval !== '')
-      return retval;
+		 
+		
+		  
+//Creamos la lista de opciones en los select de volumen
+
+const CreateListVol = (lista) => {
+	
+	
+	const optionsList = document.querySelectorAll(".ListVolumen option")
+	optionsList.forEach(o => o.remove());
+	
+	setTimeout(function(){
+		
+		const listoVolumen = document.querySelectorAll(".ListVolumen")
+
+		const options = document.createElement("option")
+		options.setAttribute('value', lista.volumen)
+		options.append(lista.descripcion)
+
+
+		listoVolumen.forEach(select => {
+			// Clona la nueva opción para evitar problemas al añadir la misma referencia
+			const optionClone = options.cloneNode(true);
+			select.add(optionClone);
+		});	
+		
+	}, 10);
+	
+	
+}
+
+
+/*Imprimimos lista de volumenes en el select*/
+const PrintListV = async () => {
+
+    try{
+		
+        const respuesta = await fetch("./script/volumen.json")
+        const listas =  await respuesta.json()
+        listas.forEach(lista => {
+           CreateListVol(lista)
+			
+        })
+		
+        return listas
+    }catch(error){
       
-    alert("Debes escribir algo para continuar");
-  }
+		Swal.fire("Tenemos problemas, intenta más tarde");
+    }
+ 	
+
+}
+
+
+const iniciar = async () => {
+    
+  	cargarListaDesdeLocalStorage();
+  	actualizarLista();
+	PrintListV()
 }
 
 // Cargar la lista desde localStorage cuando se carga la página
-window.onload = function() {
-  cargarListaDesdeLocalStorage();
-  actualizarLista();
-};
